da655a89e211c22dbe0f79ca1b6ddc10aee6094f
James Foster <james.mdb@gmail.com>
2010-04-02 21:01:40 +0100

    Added <markdown> tag to spark

--- src/Spark/Compiler/NodeVisitors/ChunkBuilderVisitor.cs
+++ src/Spark/Compiler/NodeVisitors/ChunkBuilderVisitor.cs
@@ -87,7 +87,8 @@ namespace Spark.Compiler.NodeVisitors
                                       {"macro", (n,i)=>VisitMacro(i)},
                                       {"render", VisitRender},
                                       {"section", VisitSection},
-                                      {"cache", VisitCache}
+                                      {"cache", VisitCache},
+                                      {"markdown", VisitMarkdown}
                                   };
         }
 
@@ -846,6 +847,17 @@ namespace Spark.Compiler.NodeVisitors
 
             if (frame != null)
                 frame.Dispose();
+        }
+
+        private void VisitMarkdown(SpecialNode specialNode, SpecialNodeInspector inspector)
+        {
+            var markdownChunk = new MarkdownChunk();
+
+            Chunks.Add(markdownChunk);
+            using (new Frame(this, markdownChunk.Body))
+            {
+                Accept(inspector.Body);
+            }
         }
 
         private bool SatisfyElsePrecondition()
